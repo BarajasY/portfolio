f4d5aef9b6b8a15aed005157bee0caa11cbe5faf
Yahir Moreno <yahirmb7568@gmail.com>
2022-06-21 16:39:37 -0500

    Added a new project
    -Implemented the rick and morty API web to my projects section.
    -Changed a little the projects section to allow more easy integration of new projects.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
             <div className="navbar_content">
                 <div className="test">
                     <div className="navbar_text" id="about">
-                        <h1><Link to="about_container" spy={true} smooth={true} offset={-450} duration={500}>About</Link></h1>
+                        <h1><Link to="about_container" spy={true} smooth={true} offset={-100} duration={500}>About</Link></h1>
                     </div>
                     <div className="navbar_text" id="projects">
                         <h1><Link to="projects_container" spy={true} smooth={true} offset={100} duration={500}>Projects</Link></h1>
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './Projects.css';
-import elantra_mockup from '../../assets/elantra_mockup.PNG'
-import { FaReact, FaCss3, FaHtml5, FaBootstrap } from 'react-icons/fa'
+import elantra_mockup from '../../assets/elantra_mockup.PNG';
+import RAM_API from '../../assets/RAM_API.PNG';
+import { FaReact, FaCss3, FaHtml5, FaBootstrap } from 'react-icons/fa';
+import { AiFillApi } from 'react-icons/ai'
 
 const Projects = () => {
     return (
         <div className="projects_container">
-            <div className="project">
+            <div className="projects_header">
                 <h1>Projects</h1>
+            </div>
+            <div className="projects">
                 <div className="test_project">
                     <img src={elantra_mockup} alt="elantra" className="image" />
                     <div className="buttons">
@@ -30,6 +34,32 @@ const Projects = () => {
                             <FaCss3 className="icon" />
                             <FaHtml5 className="icon" />
                             <FaBootstrap className="icon" />
+                        </div>
+                    </div>
+                </div>
+                <div className="test_project">
+                    <img src={RAM_API} alt="elantra" className="image" />
+                    <div className="buttons">
+                        <a href="https://rickandmortyapi-test.netlify.app/" target="_blank" rel="noreferrer"><button className="page_buttons">See Live</button></a>
+                        <a href="https://github.com/Kanomb/api_101" target="_blank" rel="noreferrer"><button className="page_buttons">Source code</button></a>
+                    </div>
+                    <div className="project_title">
+                        <h1>Rick and Morty API</h1>
+                    </div>
+                    <div className="project_description">
+                        <div className="description_text">
+                            <p>React website i made to get knowledge of APIS, in this case i used the public Rick and Morty API.</p>
+                            <p>For this project i used:</p>
+                            <li>React, CSS and HTML.</li>
+                            <li>useState and useEffect react hooks to fetch information from the respective URLs.</li>
+                            <li>Implementation of simple pagination to fetch more characters from the API.</li>
+                            <li>Naturally i used API implementation practices to fetch information from the API.</li>
+                        </div>
+                        <div className="description_icons">
+                            <FaReact className="icon" />
+                            <FaCss3 className="icon" />
+                            <FaHtml5 className="icon" />
+                            <AiFillApi className="icon" />
                         </div>
                     </div>
                 </div>
