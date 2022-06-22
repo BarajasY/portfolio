13829545342341169ad84cbc54a659d16a44c0b0
Yahir Moreno <yahirmb7568@gmail.com>
2022-06-22 10:57:28 -0500

    Changed Navbar
    -Made some design changes to the navbar.
    -Changed introduction name font and color.

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -34,7 +34,7 @@ const About = () => {
                     <div className="about_content active">
                         <button onClick={handleClick}>Less about me</button>
                         <div className="about_text">
-                            <h1>My name is Yahir Moreno Barajas, i'm 19 and i am a Mexican web developer looking for opportunities to improve my knowledge in the field.</h1>
+                            <h1>My name is Yahir Moreno Barajas, i'am a 19 Mexican web developer looking for opportunities to improve my knowledge in the field.</h1>
                             <h1>My interest started about 18 months ago when i took the python programming language, i mainly wanted to become a data scientist, i even learned Pandas/Seaborn/Jpt notebooks...</h1>
                             <h1>Since i relatively knew how to use python i moved into web development after about 3 months, with django, but soon realized that Javascript is the king of programming languages when trying to build a website, so i started to learn it.</h1>
                             <h1>Lastly i focused myself and realized that i wasn't going to be productive if i became first a junior backend developer, so i started to learn CSS, HTML & React to become a frontend dev and then build my knowledge up until i'm capable enough to have a Full Stack role, with enough knowledge in both sides, frontend and backend.</h1>
