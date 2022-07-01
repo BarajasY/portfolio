3c597e0d26755bfe1b9cdd4224c209d5f9ce64ec
Yahir Moreno <yahirmb7568@gmail.com>
2022-07-01 00:59:45 -0500

    Added Yanagiya project.
    -Added my latest project.

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -3,6 +3,7 @@ import './Projects.css';
 import elantra_mockup from '../../assets/elantra_mockup.PNG';
 import RAM_API from '../../assets/RAM_API.PNG';
 import portfolio_page from '../../assets/portfolio_page.PNG';
+import yanagiya_page from '../../assets/yanagiya_page.PNG';
 import { FaReact, FaCss3, FaHtml5, FaBootstrap } from 'react-icons/fa';
 import { AiFillApi } from 'react-icons/ai'
 import { IoLogoJavascript } from 'react-icons/io';
@@ -97,6 +98,32 @@ const Projects = () => {
                                 <li>React, Javascript, CSS and HTML.</li>
                                 <li>React hooks such as useState and useEffect.</li>
                                 <li>React libraries such as react-scroll, react-typed and react-icons</li>
+                            </div>
+                            <div className="description_icons">
+                                <FaReact className="icon" />
+                                <FaCss3 className="icon" />
+                                <FaHtml5 className="icon" />
+                                <IoLogoJavascript className="icon" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="test_project" >
+                        <img src={yanagiya_page} alt="portfolio" className="image" />
+                        <div className="buttons">
+                            <a href="https://yanagiya-clone.netlify.app" target="_blank" rel="noreferrer"><button className="page_buttons">See Live</button></a>
+                            <a href="https://github.com/Kanomb/yanagiya-clone" target="_blank" rel="noreferrer"><button className="page_buttons">Source code</button></a>
+                        </div>
+                        <div className="project_title">
+                            <h1>Yanagiya.mx Clone Website</h1>
+                        </div>
+                        <div className="project_description">
+                            <div className="description_text">
+                                <p>First "big" project i made, a clone website of Yanagiya. I feel that after this project i visualized the amount of work any page needs to function.</p>
+                                <p>For this project i used:</p>
+                                <li>React, Javascript, CSS and HTML.</li>
+                                <li>React hooks such as useState and useEffect.</li>
+                                <li>React Router library to asign routes to explore the website.</li>
+                                <li>Other react libraries such as react-icons, react-player, react-img-zoom</li>
                             </div>
                             <div className="description_icons">
                                 <FaReact className="icon" />
