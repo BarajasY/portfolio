bd3caba16aa98bb7254822953ec2744b5ee07863
Yahir Moreno <yahirmb7568@gmail.com>
2022-07-02 23:20:27 -0500

    Added a new project!
    -Blog webpage project.
    It is a webpage where you can login, logout with google and create/delete posts.

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -4,9 +4,11 @@ import elantra_mockup from '../../assets/elantra_mockup.PNG';
 import RAM_API from '../../assets/RAM_API.PNG';
 import portfolio_page from '../../assets/portfolio_page.PNG';
 import yanagiya_page from '../../assets/yanagiya_page.PNG';
+import blog_page from '../../assets/blog_page.PNG';
 import { FaReact, FaCss3, FaHtml5, FaBootstrap } from 'react-icons/fa';
 import { AiFillApi } from 'react-icons/ai'
 import { IoLogoJavascript } from 'react-icons/io';
+import { SiFirebase } from 'react-icons/si';
 
 const Projects = () => {
 
@@ -130,6 +132,33 @@ const Projects = () => {
                                 <FaCss3 className="icon" />
                                 <FaHtml5 className="icon" />
                                 <IoLogoJavascript className="icon" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="test_project" >
+                        <img src={blog_page} alt="portfolio" className="image" />
+                        <div className="buttons">
+                            <a href="https://yahirmb-blog.netlify.app" target="_blank" rel="noreferrer"><button className="page_buttons">See Live</button></a>
+                            <a href="https://github.com/Kanomb/blog" target="_blank" rel="noreferrer"><button className="page_buttons">Source code</button></a>
+                        </div>
+                        <div className="project_title">
+                            <h1>Blog website</h1>
+                        </div>
+                        <div className="project_description">
+                            <div className="description_text">
+                                <p>First project where i manage to use a database. In this case it is a classic blog webpage in which you can login and logout with google, create posts and delete posts.</p>
+                                <p>For this project i used:</p>
+                                <li>React, Javascript, CSS and HTML.</li>
+                                <li>Firebase to work as an authenticator and as a database.</li>
+                                <li>React hooks such as useState and useEffect.</li>
+                                <li>React libraries such as react-icons, react-router-dom.</li>
+                            </div>
+                            <div className="description_icons">
+                                <FaReact className="icon" />
+                                <FaCss3 className="icon" />
+                                <FaHtml5 className="icon" />
+                                <IoLogoJavascript className="icon" />
+                                <SiFirebase className="icon" />
                             </div>
                         </div>
                     </div>
