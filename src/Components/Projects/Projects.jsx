7aa4f9fbb6ae90bb46c3d6c6371a4206a8d459a0
Yahir Moreno <yahirmb7568@gmail.com>
2022-07-10 15:41:03 -0500

    Added a new project
    -Added astrobuyer webpage project!

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -5,6 +5,7 @@ import RAM_API from '../../assets/RAM_API.PNG';
 import portfolio_page from '../../assets/portfolio_page.PNG';
 import yanagiya_page from '../../assets/yanagiya_page.PNG';
 import blog_page from '../../assets/blog_page.PNG';
+import astrobuyer_page from '../../assets/astrobuyer_page.PNG'
 import { FaReact, FaCss3, FaHtml5, FaBootstrap } from 'react-icons/fa';
 import { AiFillApi } from 'react-icons/ai'
 import { IoLogoJavascript } from 'react-icons/io';
@@ -157,6 +158,35 @@ const Projects = () => {
                                 <li>Firebase to work as an authenticator and as a database.</li>
                                 <li>React hooks such as useState and useEffect.</li>
                                 <li>React libraries such as react-icons, react-router-dom.</li>
+                            </div>
+                            <div className="description_icons">
+                                <FaReact className="icon" />
+                                <FaCss3 className="icon" />
+                                <FaHtml5 className="icon" />
+                                <IoLogoJavascript className="icon" />
+                                <SiFirebase className="icon" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="test_project" >
+                        <img src={astrobuyer_page} alt="astrobuyer page" className="image" />
+                        <div className="buttons">
+                            <a href="https://yahirmb-blog.netlify.app" target="_blank" rel="noreferrer"><button className="page_buttons">See Live</button></a>
+                            <a href="https://github.com/Kanomb/astrobuyer" target="_blank" rel="noreferrer"><button className="page_buttons">Source code</button></a>
+                        </div>
+                        <div className="project_title">
+                            <h1>Astrobuyer website</h1>
+                            <p>100% Responsive</p>
+                        </div>
+                        <div className="project_description">
+                            <div className="description_text">
+                                <p>Webpage where you can "buy" planets and take a look at their description. My first time simulating a store site!.</p>
+                                <p>You can log in and simulate that you're buying some planets.</p>
+                                <p>For this project i used:</p>
+                                <li>React, Javascript, CSS and HTML.</li>
+                                <li>Firebase auth to allow google sign up and firestore database to store all the planets/stars.</li>
+                                <li>React Context to share information about the cart between the elements of the project.</li>
+                                <li>React hooks such as useState and useEffect.</li>
                             </div>
                             <div className="description_icons">
                                 <FaReact className="icon" />
