c2239b7bceaefd5eba1d513981343d1d91ade355
Yahir Moreno <yahirmb7568@gmail.com>
2022-07-19 18:45:53 -0500

    Fixed navbar scroll

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -14,9 +14,9 @@ const About = () => {
         <div className="about_container">
             <motion.div className="about_content" initial={{ opacity: 0, y: 50 }} whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true }} transition={{ duration: 2 }}>
                 <div className="about_text">
-                    <h1>My name is Yahir Moreno Barajas, i'am a 19 year old Mexican web developer looking for opportunities to improve my knowledge in the field.</h1>
-                    <h1>My interest started about 18 months ago when i took the python programming language, i mainly wanted to become a data scientist, i even learned Pandas/Seaborn/Jpt notebooks...</h1>
-                    <h1>Since i relatively knew how to use python i moved into web development after about 3 months, with django, but soon realized that Javascript is the king of programming languages when trying to build a website, so i started to learn it.</h1>
+                    <h1>My name is Yahir Moreno Barajas, Mexican web developer looking for opportunities to improve my knowledge in the field.</h1>
+                    <h1>My interest started about 18 months ago when i took the python language, i mainly wanted to become a data scientist, i even learned Pandas/Seaborn/Jpt notebooks...</h1>
+                    <h1>Since i relatively knew how to use python i moved into web development after about 3 months, with django, but soon realized that Javascript is the king of languages when trying to build a website, so i started to learn it.</h1>
                     <h1>Lastly i focused myself and realized that i wasn't going to be productive if i became first a junior backend developer, so i started to learn CSS, HTML & React to become a frontend dev and then build my knowledge up until i'm capable enough to have a Full Stack role, with enough knowledge in both sides, frontend and backend.</h1>
                 </div>
                 <div className="contact_section">
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -27,13 +27,13 @@ const Navbar = () => {
             <div className="navbar_content">
                 <div className="test">
                     <motion.div className="navbar_text" id="about" animate={{ x: 0 }} initial={{ x: -2000 }} transition={{ duration: 1, type: "spring", delay: .3 }}>
-                        <h1><Link to="about_container" spy={true} smooth={true} offset={-200} duration={500}>About</Link></h1>
+                        <h1><Link to="about_container" spy={true} smooth={true} offset={-50} duration={500}>About</Link></h1>
                     </motion.div>
                     <motion.div className="navbar_text" id="projects" animate={{ x: 0 }} initial={{ x: -2000 }} transition={{ delay: .2, duration: 1, type: "spring" }}>
-                        <h1><Link to="projects_container" spy={true} smooth={true} offset={-300} duration={500}>Projects</Link></h1>
+                        <h1><Link to="projects_container" spy={true} smooth={true} offset={0} duration={500}>Projects</Link></h1>
                     </motion.div>
                     <motion.div className="navbar_text" id="Contact" animate={{ x: 0 }} initial={{ x: -2000 }} transition={{ delay: .1, duration: 1, type: "spring" }}>
-                        <h1><Link to="contact_container" spy={true} smooth={true} offset={-1000} duration={500}>Contact</Link></h1>
+                        <h1><Link to="contact_container" spy={true} smooth={true} offset={100} duration={500}>Contact</Link></h1>
                     </motion.div>
                 </div>
             </div>
