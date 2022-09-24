8f471ca5c211f7b688dd14b49eda084459369f1b
Yahir Moreno <yahirmb7568@gmail.com>
2022-09-23 21:17:23 -0500

    Updated logo animation

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,13 +9,13 @@ const Navbar = () => {
         <div className="navbar_container">
             <div className="navbar_content">
                 <div className="logo">
-                    <img src={logo} alt="My logo" />
+                    <motion.img animate={{ opacity: 1 }} initial={{ opacity: 0 }} transition={{ duration: 1, delay: 1 }} src={logo} alt="My logo" />
                 </div>
                 <div className="test">
                     <motion.div className="navbar_text" id="github" animate={{ opacity: 1 }} initial={{ opacity: 0 }} transition={{ duration: 1, delay: 1 }}>
                         <h1><a href="https://github.com/Kanomb" target="_blank" rel="noreferrer"><AiFillGithub /></a></h1>
                     </motion.div>
-                    <motion.div className="navbar_text" id="linkedin" animate={{ opacity: 1 }} initial={{ opacity: 0 }} transition={{ duration: 1, delay: 1 }}>
+                    <motion.div className="navbar_text" id="linkedin" animate={{ opacity: 1 }} initial={{ opacity: 0 }} transition={{ duration: 1, delay: 1.2 }}>
                         <h1><a href="https://www.linkedin.com/in/yahirmb" target="_blank" rel="noreferrer"><AiFillLinkedin /></a></h1>
                     </motion.div>
                 </div>
