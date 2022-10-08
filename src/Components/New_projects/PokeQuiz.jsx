536428767f16fa126690f271ecd99658a702a80a
Yahir Moreno <yahirmb7568@gmail.com>
2022-10-08 12:37:39 -0500

    Added image for PokeQuiz page

--- src/Components/New_projects/PokeQuiz.jsx
+++ src/Components/New_projects/PokeQuiz.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './Projects.css';
 import pokequiz from '../../assets/pokequiz_logo.png';
+import pokequiz_page from '../../assets/pokequiz_page.PNG';
 import { motion, AnimatePresence } from 'framer-motion';
 import { FaReact, FaCss3, FaHtml5 } from 'react-icons/fa';
 import { AiOutlineClose } from 'react-icons/ai';
@@ -20,6 +21,7 @@ const PokeQuiz = () => {
                         <AiOutlineClose className="arrow" onClick={() => setOpen(!Open)} />
                         <div className="project_header">
                             <h1>Pokemon Quiz</h1>
+                            <img src={pokequiz_page} alt={pokequiz_page} id='screenshot' />
                         </div>
                         <div className="project_description">
                             <p>Another single page application using pure react and javascript, and a little bit of firebase backend.
