5de70698d4657f0ca783a2dd9d8cee1a9692671f
Yahir Moreno Barajas <100240827+Kanomb@users.noreply.github.com>
2023-02-15 17:31:08 -0600

    Improved responsiveness
    Added Pokechampions project to original visible list of projects.

--- src/Components/New_projects/Projects.jsx
+++ src/Components/New_projects/Projects.jsx
@@ -48,6 +48,7 @@ const Projects = () => {
                         <Togenim />
                         <Astrobuyer />
                         <Acceleracers/>
+                        <Pokechampions />
                     </section>
                 }
             </div>
